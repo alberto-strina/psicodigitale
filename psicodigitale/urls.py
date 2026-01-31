8a4aeef0496bf14d18ad3999aa17537be6f0d238
Alberto Strina <alberto.strina@gmail.com>
2026-02-01 00:08:41 +0100

    fixing static

--- psicodigitale/urls.py
+++ psicodigitale/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic.base import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
+    static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0]),
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/admin/', permanent=False)),
 ]
