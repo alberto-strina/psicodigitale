d8db37909f845685140705ce50c0f0272c2f6441
Alberto Strina <alberto.strina@gmail.com>
2026-01-31 21:07:28 +0100

    added gunicorn

--- psicodigitale/settings.py
+++ psicodigitale/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -131,3 +133,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TYPEFORM_TOKEN = 'tfp_EMKejt7D7t4emKwM2zPtHZ9UG3vTLNySMdjQXj7y84Hk_dKNhipQpYu7Y'
